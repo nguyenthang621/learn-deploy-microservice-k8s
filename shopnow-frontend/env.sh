1aab4d13731888423239250a35eb71ebde1be2ed
nguyenthang621 <nthang621@gmail.com>
2025-03-14 14:18:53 +0700

    change Dockerfile frontend to dynamic env var

---
+++ shopnow-frontend/env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2024. Devtron Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Recreate config file
+rm -rf ./env-config.js
+touch ./env-config.js
+
+# Add assignment 
+echo "window._env_ = {" >> ./env-config.js
+
+# Read each line in .env file
+# Each line represents key=value pairs
+while read -r line || [[ -n "$line" ]];
+do
+  # Split env variables by character `=`
+  if printf '%s\n' "$line" | grep -q -e '='; then
+    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  fi
+
+  # Read value of current variable if exists as Environment variable
+  value=$(printf '%s\n' "${!varname}")
+  # Otherwise use value from .env file
+  [[ -z $value ]] && value=${varvalue}
+  
+  # Append configuration property to JS file
+  if [[ "$value" == "true" ]] || [[ "$value" == "false" ]]; then
+    echo "  $varname: $value," >> ./env-config.js
+  else
+    echo "  $varname: \"$value\"," >> ./env-config.js
+  fi
+done < .env
+echo "}" >> ./env-config.js
